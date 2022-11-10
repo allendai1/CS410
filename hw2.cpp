5bfb5b18ad2013111977ad3fb35e27e208342f14
Allen Dai <allen@Allens-MacBook-Air.local>
2022-11-09 19:28:57 -0500

    c

--- hw2.cpp
+++ hw2.cpp
@@ -32,27 +32,21 @@ vector<int> extractIntegerWords(string str)
 struct creature
 {
     string name;
-    string saying;
-    int card_length;
-    int card_width;
-    vector<string> list_of_sayings;
-    creature(string name,string saying,  int card_length, int card_width, vector<string> sayings)
+    vector<int> dimensions;
+    creature(string name,vector<int> dimensions)
     {
         this->name = name;
-        this->saying = saying;
-        this->card_length = card_length;
-        this->card_width = card_width;
-        this->list_of_sayings = sayings;
+        this->dimensions = dimensions;
+        // this->saying = saying;
     }
 
-    string randomSaying(){
-        // srand(37);
-        int randomIndex = rand() % list_of_sayings.size();
-        return list_of_sayings[randomIndex];
-
-    }
+    
 
 };
+int randomIndex(int i, int j){
+    int random = i + (rand() % j);
+    return random;
+}
 int main()
 {
     ifstream creatures_file("creatures.txt", ifstream::in);
@@ -69,23 +63,23 @@ int main()
     while(getline(sayings_file, saying)){
         sayings.push_back(saying);
     }
+
     // parse each line for name and dimensions
     while (getline(creatures_file, creature_line))
     {
         string name = creature_line.substr(0, creature_line.find(doublespace));
         creature_name = name;
-
         vector<int> dimensions = extractIntegerWords(creature_line);
         saying = rand() % sayings.size();
-        creature c = creature(creature_name,saying, dimensions[0], dimensions[1], sayings);
-        
+        // creature c = creature(creature_name,saying, dimensions[0], dimensions[1], sayings);
+        creature c = creature(creature_name, dimensions);
         creatures.push_back(c);
-        
-
     }
-    for(creature x  : creatures){
-        cout<< x.randomSaying()+"\n";
-    }
+    // for(creature c : creatures){
+    //     cout << c.name << " " << c.dimensions[0] << " " <<c.dimensions[1] << "\n";
+    // }
+    srand((unsigned) (int) 37);
+    cout << randomIndex(0, 14);
 
 
     
